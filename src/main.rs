3fcd77edd7c562e98b7602f2d7e227c9fd84e831
Peter Steidel <psteidelprogramming@gmail.com>
2022-07-23 12:21:22 -0400

    Swapchain recreation and dynamic viewport

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,43 @@
 mod engine;
-mod pipeline;
 mod mesh;
+mod pipeline;
 
 use engine::VkEngine;
 
 use winit::{
-    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
-    event_loop::ControlFlow,
-    platform::run_return::EventLoopExtRunReturn,
+	event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+	event_loop::ControlFlow,
 };
+use winit::{event_loop::EventLoop, window::WindowBuilder};
 
 fn main() {
-    let engine = VkEngine::new();
+	let event_loop = EventLoop::new();
+	let window = WindowBuilder::new()
+		.with_title("Ecocide")
+		.with_inner_size(winit::dpi::LogicalSize::new(800.0f64, 600.0f64))
+		.build(&event_loop)
+		.unwrap();
+	let mut engine = VkEngine::new(&window);
 
-    engine
-        .event_loop
-        .borrow_mut()
-        .run_return(|event, _, control_flow| {
-            *control_flow = ControlFlow::Poll;
-            match event {
-                Event::WindowEvent {
-                    event:
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                                KeyboardInput {
-                                    state: ElementState::Pressed,
-                                    virtual_keycode: Some(VirtualKeyCode::Escape),
-                                    ..
-                                },
-                            ..
-                        },
-                    ..
-                } => *control_flow = ControlFlow::Exit,
-                Event::MainEventsCleared => engine.draw(),
-                _ => (),
-            }
-        });
+	event_loop.run(move |event, _, control_flow| {
+		*control_flow = ControlFlow::Poll;
+		match event {
+			Event::WindowEvent {
+				event:
+					WindowEvent::CloseRequested
+					| WindowEvent::KeyboardInput {
+						input:
+							KeyboardInput {
+								state: ElementState::Pressed,
+								virtual_keycode: Some(VirtualKeyCode::Escape),
+								..
+							},
+						..
+					},
+				..
+			} => *control_flow = ControlFlow::Exit,
+			Event::MainEventsCleared => engine.draw(),
+			_ => (),
+		}
+	});
 }
